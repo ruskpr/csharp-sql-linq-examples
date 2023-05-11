373da635ef2f8664e3158ac34a544132aeec00f3
Rustu Koprulu <rustulp@gmail.com>
2023-05-11 13:47:05 -0600

    fix hyperlink

--- LinqExamples/Pages/HomePage.xaml.cs
+++ LinqExamples/Pages/HomePage.xaml.cs
@@ -1,7 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
 namespace LinqExamples.pages;
 
 public partial class HomePage : Page
 {
     public HomePage() => InitializeComponent();
+
+    private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+        e.Handled = true;
+    }
 }
